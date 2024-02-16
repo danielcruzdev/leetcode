58dfb6043aabfcaccad1b7fe816dcd7472c148a7
Daniel Cruz <d.afc98@gmail.com>
2024-02-15 22:59:48 -0300

    <Solved>[CommonPrefix] - problem solved and posted on leetcode

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LongestCommonPrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            string[] strs = new string[] { "flower", "flow", "flight" };
+            var prefix = solution.LongestCommonPrefix(strs);
+
+            Console.WriteLine($"Prefix: {prefix}");
+        }
+    }
+
+    //Solution posted in LeetCode :)
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            string shortest = strs.OrderBy(s => s.Length).First();
+
+            for (int i = 0; i < shortest.Length; i++)
+            {
+                if (strs.Select(s => s[i]).Distinct().Count() > 1) 
+                    return shortest[..i];
+            }
+
+            return shortest;
+        }
+    }
+}

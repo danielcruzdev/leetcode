5b6058dde7080ab8e8ff2a5e04c0412771b2c910
Daniel Cruz <d.afc98@gmail.com>
2024-02-26 23:06:50 -0300

    <Solved>[FindTheIndexString] - problem solved and posted on leetcode

---
+++ FindTheIndexString/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FindTheIndexString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            string haystack = "aabaaabaaac";
+            string needle = "aabaaac";
+
+            var response = solution.StrStr(haystack, needle);
+
+            Console.WriteLine(response);
+        }
+    }
+
+    //Solution posted in LeetCode :)
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length > haystack.Length) return -1;
+            
+            for (int i = 0; i <= haystack.Length - needle.Length; ++i)
+            {
+                if (haystack.Substring(i, needle.Length) == needle)
+                    return i;         
+            }
+
+            return -1;
+        }
+    }
+}

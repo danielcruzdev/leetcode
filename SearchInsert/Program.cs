a901b50a01304d67591d35e7c3cf47f2270a854e
Daniel Cruz <d.afc98@gmail.com>
2024-02-26 23:22:54 -0300

    <Solved>[SearchInsert] - problem solved and posted on leetcode

---
+++ SearchInsert/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SearchInsert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            int[] nums = { 1, 3, 5, 6 };
+            int target = 4;
+
+            var response = solution.SearchInsert(nums, target);
+
+            Console.WriteLine(response);
+        }
+    }
+
+    //Solution posted in LeetCode :)
+    public class Solution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target)
+                    return i;
+            }
+
+            return nums.Length;
+        }
+    }
+}

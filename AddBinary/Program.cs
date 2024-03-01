fd417c238b756b633d7b51874548365a898d005e
Daniel Cruz <d.afc98@gmail.com>
2024-02-29 23:25:35 -0300

    <Solved>[AddBinary] - problem solved and posted on leetcode

---
+++ AddBinary/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace AddBinary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var a = "1111";
+            var b = "1000";
+
+            var response = solution.AddBinary(a, b);
+
+            Console.WriteLine(response);
+        }
+    }
+
+    //Solution posted in LeetCode :)
+    public class Solution
+    {
+        public string AddBinary(string a, string b)
+        {
+            int carry = 0;
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+            string result = "";
+
+            while (i >= 0 || j >= 0 || carry > 0)
+            {
+                int sum = carry;
+                if (i >= 0)
+                {
+                    sum += a[i] - '0';
+                    i--;
+                }
+                if (j >= 0)
+                {
+                    sum += b[j] - '0';
+                    j--;
+                }
+
+                result = (sum % 2) + result;
+                carry = sum / 2;
+            }
+
+            return result;
+        }
+    }
+}
